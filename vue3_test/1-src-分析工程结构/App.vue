a3496cf3e6c28f420e93e468b478d4454d4e6793
Orange <3434782480@qq.com>
2024-05-15 14:42:39 +0800

    尚硅谷-张天禹，vue2-vu3，136-141

---
+++ vue3_test/1-src-分析工程结构/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <!-- vue3组件中的模板结构由可以没有跟标签 -->
+  <img alt="Vue logo" src="./assets/logo.png">
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
