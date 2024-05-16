44729980d05ec72a85d58fbd905df8226862441e
Orange <3434782480@qq.com>
2024-05-16 16:30:04 +0800

    尚硅谷-张天禹，vue2-vue3，142-145

---
+++ vue3_test/2-src-初识setup/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <!-- 在vue3中可以使用vue2中的语法 -->
+  <!-- 在vue3中使用vue2的方法读取相同字段的数据会以vue3的为主 -->
+  <h1>一个人的信息</h1>
+  <h2>姓名：{{ name }}</h2>
+  <h2>年龄：{{ age }}</h2>
+  <button @click="sayHello">说话</button>
+</template>
+
+<script>
+import { h } from 'vue';
+
+export default {
+  name: 'App',
+  // 此处仅用于测试，不考虑响应式的问题
+  setup() {
+    // 数据
+    let name = '张三';
+    let age = 18;
+
+    // 方法
+    function sayHello() {
+      console.log(`我叫${name}, 我今年${age}岁了，你好啊！`);
+    }
+
+    // 返回一个对象（常用）
+    return {
+      name,
+      age,
+      sayHello
+    }
+
+    // 返回一个函数（渲染函数）
+    // return () => h('h1', '尚硅谷')
+  }
+}
+</script>
+
+<style>
+/* #app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+} */
+</style>
--- vue3_test/3-src-ref函数/App.vue
+++ vue3_test/3-src-ref函数/App.vue
@@ -1,36 +1,26 @@
 <template>
   <h1>一个人的信息</h1>
-  <h2>姓名：{{ person.name }}</h2>
-  <h2>年龄：{{ person.age }}</h2>
-  <h2>工作种类：{{ person.job.type }}</h2>
-  <h2>爱好：{{ person.hobby }}</h2>
-  <h2>测试的数据C: {{ person.job.a.b.c }}</h2>
+  <h2>姓名：{{ name }}</h2>
+  <h2>年龄：{{ age }}</h2>
+  <h2>工作种类：{{ job.type }}</h2>
+  <h2>薪水：{{ job.salary }}</h2>
   <button @click="changeInfo">修改个人信息</button>
 </template>
 
 <script>
-import { reactive } from 'vue';
+import { ref } from 'vue';
 
 export default {
   name: 'App',
   // 此处仅用于测试，不考虑响应式的问题
   setup() {
     // 数据
-    let person = reactive({
-      name: '张三',
-      age: 18,
-      job: {
-        type: '前端工程师',
-        salary: '30k',
-        a: {
-          b: {
-            c: 666
-          }
-        }
-      },
-      hobby: ['抽烟', '喝酒', '烫头']
+    let name = ref('张三');
+    let age = ref(18);
+    let job = ref({
+      type: '前端工程师',
+      salary: '30k'
     });
-
 
     // 方法
     function changeInfo() {
@@ -38,17 +28,19 @@ export default {
       // ref: 引用对象、引用实现对象
       // RefImpl
       // Ref：reference 引用，Impl: implement 实现
-      person.name = '李四';
-      person.age = 20;
-      person.job.type = 'UI设计师';
-      person.job.salary = '50k';
-      person.job.a.b.c = 999;
-      person.hobby[0] = '学习';
+      // name.value = '李四';
+      // age.value = 20;
+      job.value.type = 'UI设计师';
+      job.value.salary = '50k';
+      console.log(job.value);
+
     }
 
     // 返回一个对象（常用）
     return {
-      person,
+      name,
+      age,
+      job,
       changeInfo
     }
   }
