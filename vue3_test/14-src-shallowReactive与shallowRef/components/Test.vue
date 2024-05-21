cd3230e3698a81d4a7fcb212b8cfe27d51b65d51
Orange <3434782480@qq.com>
2024-05-21 17:12:20 +0800

    尚硅谷-张天禹，158，shallowReactive与shallowRef

---
+++ vue3_test/14-src-shallowReactive与shallowRef/components/Test.vue
@@ -0,0 +1,27 @@
+<template>
+  <h2>我是test组件</h2>
+  {{ point }}
+</template>
+
+<script>
+import usePoint from '../hooks/usePoint'
+
+export default {
+  name: 'Test',
+  setup() {
+    const point = usePoint()
+
+    return {
+      point
+    }
+  }
+
+
+}
+
+
+
+
+</script>
+
+<style lang="scss" scoped></style>
